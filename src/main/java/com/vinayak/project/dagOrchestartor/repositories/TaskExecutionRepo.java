0727798faf29ebdef20308be7c20e1514d4cc220
vinayak <vinayak122001@gmail.com>
2026-04-19 13:42:29 +0530

    Import Cleanup

--- src/main/java/com/vinayak/project/dagOrchestartor/repositories/TaskExecutionRepo.java
+++ src/main/java/com/vinayak/project/dagOrchestartor/repositories/TaskExecutionRepo.java
@@ -3,7 +3,6 @@ package com.vinayak.project.dagOrchestartor.repositories;
 import com.vinayak.project.dagOrchestartor.entities.TaskExecution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.List;
 
 public interface TaskExecutionRepo extends JpaRepository<TaskExecution,Long> {
